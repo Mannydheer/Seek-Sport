0eb603dc0d2636125ac4ab4c7045c74f18f48450
Manny Dheer <mannydheer@gmail.com>
2020-05-06 14:22:46 -0400

    temp fix for time conflict backend

--- client/src/components/Host/Host.js
+++ client/src/components/Host/Host.js
@@ -93,7 +93,7 @@ const validateDateBooking = (startDate) => {
 
 
 
-
+//-----------------------------------COMPONENT---------------------------------
 const Host = () => {
 
     //for modal class.
