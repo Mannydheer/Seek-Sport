58ac3eea028908f9000516c9051dc49cf62e0bfc
Manny Dheer <mannydheer@gmail.com>
2020-05-23 16:46:50 -0400

    feat: handleGetChatRoom controller added. No need for repo and service.

--- server/controllers/handlers/chatController.js
+++ server/controllers/handlers/chatController.js
@@ -4,6 +4,11 @@ const collectionEvents = "Events";
 const collectionParticipants = "Participants";
 const { getConnection } = require("../../connection/connection");
 
+const {
+  getEventById,
+  getParticipantsById,
+} = require("../../services/joinLeaveCancelService");
+
 var ObjectId = require("mongodb").ObjectID;
 //env vairablkes
 require("dotenv").config();
@@ -12,29 +17,43 @@ require("dotenv").config();
 //@desc get chat room ID for specified EVENT.
 //@access PRIVATE - will need to validate token? YES - add...
 const handleGetChatRoom = async (req, res, next) => {
-  const eventId = req.params.eventId;
   try {
-    const db = getConnection().db(dbName);
-    let eventData = await db
-      .collection(collectionEvents)
-      .findOne({ _id: ObjectId(eventId) });
+    const eventId = req.params.eventId;
+    if (!eventId) {
+      return res
+        .status(400)
+        .json({ status: 400, message: "Missing event Id for chat room." });
+    }
+    // const db = getConnection().db(dbName);
+    let eventData = await getEventById(eventId);
+    // let eventData = await db
+    //   .collection(collectionEvents)
+    //   .findOne({ _id: ObjectId(eventId) });
     //since we are making a participant ID as soon as we host an event.
     //so check if there is no event.
     if (!eventData) {
-      res.status(404).json({ status: 404, message: "There are no events." });
-    } else {
-      let participantData = await db
-        .collection(collectionParticipants)
-        .findOne({ _id: ObjectId(eventData.participantId) });
-      //since the participantId is the room ID...
-      res.status(200).json({
-        status: 200,
-        message:
-          "Success getting participantID events associated with the event.",
-        participantId: eventData.participantId,
-        eventParticipants: participantData.participants,
+      return res
+        .status(404)
+        .json({ status: 404, message: "There are no events." });
+    }
+    let participantData = await getParticipantsById(eventData.participantId);
+    if (!participantData) {
+      return res.status(404).json({
+        status: 404,
+        message: "There are no participants for this event.",
       });
     }
+    // let participantData = await db
+    //   .collection(collectionParticipants)
+    //   .findOne({ _id: ObjectId(eventData.participantId) });
+    //since the participantId is the room ID...
+    res.status(200).json({
+      status: 200,
+      message:
+        "Success getting participantID events associated with the event.",
+      participantId: eventData.participantId,
+      eventParticipants: participantData.participants,
+    });
   } catch (error) {
     console.log(error.stack, "Catch Error in handleGetChatRoom ");
     res.status(500).json({ status: 500, message: error.message });
