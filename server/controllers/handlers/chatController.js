d33253391a5ea4b1ba3637e9a5139b23864d9d99
Manny Dheer <mannydheer@gmail.com>
2020-05-27 12:22:58 -0400

    fix: chatController - custom error handling fix

--- server/controllers/handlers/chatController.js
+++ server/controllers/handlers/chatController.js
@@ -3,7 +3,7 @@ const {
   getParticipantsById,
 } = require("../../services/joinLeaveCancelService");
 
-const { UnauthorizedError, NotFoundError } = require("../../utils/errors");
+const { NotFoundError, BadRequestError } = require("../../utils/errors");
 //@endpoint GET /getChatRoom
 //@desc get chat room ID for specified EVENT.
 //@access PRIVATE - will need to validate token? YES - add...
@@ -11,21 +11,18 @@ const handleGetChatRoom = async (req, res, next) => {
   try {
     const eventId = req.params.eventId;
     if (!eventId) {
-      let err = new UnauthorizedError("Missing event Id for chat room.");
-      next(err);
+      throw new BadRequestError("Missing event Id for chat room.");
     }
     // const db = getConnection().db(dbName);
     let eventData = await getEventById(eventId);
     if (!eventData) {
-      let err = new NotFoundError(
+      throw new NotFoundError(
         "Unable to retrieve event data for handleGetChatRoom."
       );
-      next(err);
     }
     let participantData = await getParticipantsById(eventData.participantId);
     if (!participantData) {
-      let err = new NotFoundError("There are no participants for this event.");
-      next(err);
+      throw new NotFoundError("There are no participants for this event.");
     }
     res.status(200).json({
       status: 200,
@@ -34,9 +31,8 @@ const handleGetChatRoom = async (req, res, next) => {
       participantId: eventData.participantId,
       eventParticipants: participantData.participants,
     });
-  } catch (error) {
-    console.log(error.stack, "Catch Error in handleGetChatRoom ");
-    res.status(500).json({ status: 500, message: error.message });
+  } catch (err) {
+    next(err);
   }
 };
 module.exports = {
