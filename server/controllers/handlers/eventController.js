cf7b33c8bc5b015cd85f349e0d1a0cc41f5940c0
Manny Dheer <mannydheer@gmail.com>
2020-05-27 12:37:35 -0400

    feat: eventController custom errors added

--- server/controllers/handlers/eventController.js
+++ server/controllers/handlers/eventController.js
@@ -10,21 +10,26 @@ const {
   getParticipantsById,
 } = require("../../services/joinLeaveCancelService");
 
+const { NotFoundError, BadRequestError } = require("../../utils/errors");
+
 //@endpoint GET /getEvents
 //@desc all events on the map.
 //@access PUBLIC
 const handleGetEvents = async (req, res, next) => {
   try {
     let allEvents = await getAllEvents();
-
+    if (!allEvents) {
+      throw new NotFoundError(
+        "Unable to retrieve all events in handleGetEvents event controller."
+      );
+    }
     return res.status(200).json({
       status: 200,
       message: "Success getting all events!",
       events: allEvents,
     });
-  } catch (error) {
-    console.log(error.stack, "Catch Error in handleEvents");
-    res.status(500).json({ status: 500, message: error.message });
+  } catch (err) {
+    next(err);
   }
 };
 
@@ -35,23 +40,22 @@ const handleUserEvents = async (req, res, next) => {
   try {
     const _id = req.params._id;
     if (!_id) {
-      return res.status(400).json({ status: 400, message: "Missing id." });
+      throw new BadRequestError("Missing id in handleUserEvents");
     }
     let allData = await getEventsAssociatedWithUser(_id);
     if (!allData) {
-      return res
-        .status(404)
-        .json({ status: 404, message: "There are no events booked." });
+      throw new NotFoundError(
+        "There are no events booked in handleUserEvents."
+      );
     }
     //function will push all participantId for each event into an array.
-    let participants = await allParticipantsArray(allData);
+    let participants = allParticipantsArray(allData);
     //this will return all participants for each event. Now there is access to all participants for each event the user is participanting in.
     let participantData = await getParticipantData(participants);
     if (!participantData) {
-      return res.status(404).json({
-        status: 404,
-        message: "There are no participants for this event.",
-      });
+      throw new NotFoundError(
+        "There are no participants for this event in handleUserEvents"
+      );
     }
     return res.status(200).json({
       status: 200,
@@ -60,9 +64,8 @@ const handleUserEvents = async (req, res, next) => {
       events: allData,
       participants: participantData,
     });
-  } catch (error) {
-    console.log(error.stack, "Catch Error in handleUserevents");
-    res.status(500).json({ status: 500, message: error.message });
+  } catch (err) {
+    next(err);
   }
 };
 
@@ -72,30 +75,26 @@ const handleUserEvents = async (req, res, next) => {
 const handleCurrentEventParticipants = async (req, res, next) => {
   try {
     let participantId = req.params.participantId;
-
     if (!participantId) {
-      return res
-        .status(400)
-        .json({ status: 400, message: "Missing participant Id." });
+      throw new BadRequestError(
+        "Missing participant Id in handleCurrentEventParticipants"
+      );
     }
     //insert the hosting info into DB
     let participantData = await getParticipantsById(participantId);
     if (!participantData) {
-      return res.status(400).json({
-        status: 400,
-        message: "Currently no participants registered for this event.",
-      });
+      throw new NotFoundError(
+        "Currently no participants registered for this event in handleCurrentEventParticipants"
+      );
     }
+    //success.
     return res.status(200).json({
       status: 200,
       message: "Success getting participants for single event.",
       eventParticipants: participantData.participants,
     });
-
-    //
-  } catch (error) {
-    console.log(error.stack, "Catch Error in handleViewActivityevents");
-    res.status(500).json({ status: 500, message: error.message });
+  } catch (err) {
+    next(err);
   }
 };
 
@@ -108,22 +107,19 @@ const handleSelectedParkEvents = async (req, res, next) => {
   try {
     let parkId = req.params.parkId;
     if (!parkId) {
-      return res.status(400).json({ status: 400, message: "Missing park Id." });
+      throw new BadRequestError("Missing park Id in handleSelectedParkEvents.");
     }
     let allEventsForAssociatedPark = await getEventsAssociatedWithPark(parkId);
     if (!allEventsForAssociatedPark) {
-      return res
-        .status(400)
-        .json({ status: 400, message: "No events associated with this park." });
+      throw new NotFoundError("No events associated with this park.");
     }
     return res.status(200).json({
       status: 200,
       message: "Success getting all events for selected park!",
       events: allEventsForAssociatedPark,
     });
-  } catch (error) {
-    console.log(error.stack, "Catch Error in handleSelectedParkEvents");
-    res.status(500).json({ status: 500, message: error.message });
+  } catch (err) {
+    next(err);
   }
 };
 
